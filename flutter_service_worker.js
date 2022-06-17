795534aab4cbe0da0692be3bcb776459bea3b640
wswszhys <wswszhys@163.com>
2022-06-18 07:44:02 +0800

    *.

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -49,9 +49,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "5db9a7695cd9b83f3535368c403162ca",
-"/": "5db9a7695cd9b83f3535368c403162ca",
-"main.dart.js": "3c5aeeb4763617f17300203449ca8a81",
+"index.html": "8edca37e205864c7c36dac64f70eb80d",
+"/": "8edca37e205864c7c36dac64f70eb80d",
+"main.dart.js": "68b1cb2017f115c4096ba06127e9264d",
 "manifest.json": "1ab4f3522f045fda668d45be95d40540",
 "version.json": "f50ececd41948be4f01bc2354d895f20"
 };
